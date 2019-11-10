badfac424e72595e481a0a7e947e1f77f9d77bca
Leonardo Zanone <leonardozanone@hotmail.com>
2019-11-10 17:34:55 -0300

    Fixing auth controller

--- Backend-Web/Controllers/AuthController.cs
+++ Backend-Web/Controllers/AuthController.cs
@@ -6,7 +6,7 @@ using System.Web.Http;
 
 namespace Backend_Web.Controllers
 {
-    public class AuthController : ApiController
+    public class AuthController : BaseController<AuthService>
     {
         [HttpPost]
         [AllowAnonymous]
